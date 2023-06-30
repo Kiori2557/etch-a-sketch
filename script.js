4ba371d0fb8ce5dd591dc9dc55a338ce95bf9f22
Aung Khant Paing <akpaing99@gmail.com>
2023-06-30 14:32:39 +0630

    Add buttons for grid div number&related function
    Add three button of three different gird no.
    Add the event listener to buttons and generate corresponding grid number
    Add resetDivNumber to event listener to remove the last generated div

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const container = document.querySelector(".container");
+const low = document.querySelector("#low");
+const normal = document.querySelector("#normal");
+const high = document.querySelector("#high");
 
 function createGrid(gridSquare) {
   for (let i = 0; i < gridSquare; i++) {
@@ -13,13 +16,37 @@ function createDiv(divNumber) {
     div = div.setAttribute("class", "row");
   }
 }
-
-createDiv(10);
-const rows = document.querySelectorAll(".row");
-rows.forEach((row) => {
-  for (let i = 0; i < rows.length; i++) {
-    let subDiv = document.createElement("div");
-    row.appendChild(subDiv);
-    subDiv - subDiv.setAttribute("class", "subDiv");
-  }
+function createSubDiv() {
+  window.rows = document.querySelectorAll(".row");
+  rows.forEach((row) => {
+    for (let i = 0; i < rows.length; i++) {
+      let subDiv = document.createElement("div");
+      row.appendChild(subDiv);
+      subDiv - subDiv.setAttribute("class", "subDiv");
+      subDiv.addEventListener("mouseenter", () => {
+        subDiv.style.backgroundColor = "black";
+      });
+    }
+  });
+}
+function resetDivNumber() {
+  createDiv(0);
+  createSubDiv();
+  rows.forEach((row) => row.remove());
+}
+let divNumber = 0;
+low.addEventListener("click", () => {
+  resetDivNumber();
+  createDiv(6);
+  createSubDiv();
+});
+normal.addEventListener("click", () => {
+  resetDivNumber();
+  createDiv(32);
+  createSubDiv();
+});
+high.addEventListener("click", () => {
+  resetDivNumber();
+  createDiv(64);
+  createSubDiv();
 });
