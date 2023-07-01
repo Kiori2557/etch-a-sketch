28a7dca5aa928d35f3ac00c5ee940feb88d98888
Aung Khant Paing <akpaing99@gmail.com>
2023-07-01 13:22:09 +0630

    Add a clean button and clean function
    Clean button listen to click and run the clean function
    Clean function changes the background color of the div back to white

--- script.js
+++ script.js
@@ -2,51 +2,57 @@ const container = document.querySelector(".container");
 const low = document.querySelector("#low");
 const normal = document.querySelector("#normal");
 const high = document.querySelector("#high");
+const cleanBtn = document.querySelector("#cleanBtn");
 
 function createGrid(gridSquare) {
   for (let i = 0; i < gridSquare; i++) {
     container.appendChild(div);
   }
 }
-
-function createDiv(divNumber) {
-  for (let i = 0; i < divNumber; i++) {
-    let div = document.createElement("div");
-    container.appendChild(div);
-    div = div.setAttribute("class", "row");
-  }
-}
 function createSubDiv() {
   window.rows = document.querySelectorAll(".row");
   rows.forEach((row) => {
     for (let i = 0; i < rows.length; i++) {
-      let subDiv = document.createElement("div");
+      const subDiv = document.createElement("div");
       row.appendChild(subDiv);
-      subDiv - subDiv.setAttribute("class", "subDiv");
+      subDiv.setAttribute("class", "subDiv");
       subDiv.addEventListener("mouseenter", () => {
         subDiv.style.backgroundColor = "black";
       });
     }
   });
 }
+function clean() {
+  const subDivs = document.querySelectorAll(".subDiv");
+  subDivs.forEach((subDiv) => (subDiv.style.backgroundColor = "white"));
+}
+function createRow(divNumber) {
+  for (let i = 0; i < divNumber; i++) {
+    let div = document.createElement("div");
+    container.appendChild(div);
+    div.setAttribute("class", "row");
+  }
+}
+
 function resetDivNumber() {
-  createDiv(0);
   createSubDiv();
   rows.forEach((row) => row.remove());
 }
 let divNumber = 0;
 low.addEventListener("click", () => {
   resetDivNumber();
-  createDiv(6);
+  createRow(6);
   createSubDiv();
 });
 normal.addEventListener("click", () => {
   resetDivNumber();
-  createDiv(32);
+  createRow(32);
   createSubDiv();
 });
 high.addEventListener("click", () => {
   resetDivNumber();
-  createDiv(64);
+  createRow(64);
   createSubDiv();
 });
+
+cleanBtn.addEventListener("click", clean);
